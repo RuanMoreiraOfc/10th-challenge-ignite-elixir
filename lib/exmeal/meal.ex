620b7f453d0837ad26f692f090af278d02c4546b
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-29 09:35:20 -0300

    feat: create `Meal` schema

--- lib/exmeal/meal.ex
+++ lib/exmeal/meal.ex
@@ -5,15 +5,33 @@ defmodule Exmeal.Meal do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
-
-  @required_params [:description, :date, :calories, :user_id]
-  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+  @displayable_params [
+    :description,
+    :date,
+    :calories
+  ]
+  @params @displayable_params
+  @derive {Jason.Encoder, only: [:id] ++ @displayable_params}
 
   schema "meals" do
-    # TO DO
+    field(:description, :string)
+    field(:date, :date)
+    field(:calories, :integer)
   end
 
-  def changeset() do
-    # TO DO
+  def changeset(params) do
+    %__MODULE__{}
+    |> build_changeset(params, @params)
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> build_changeset(params, @params)
+  end
+
+  defp build_changeset(struct, params, fields) do
+    struct
+    |> cast(params, fields)
+    |> validate_required(fields)
   end
 end
