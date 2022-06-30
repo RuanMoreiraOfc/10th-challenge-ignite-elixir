0c77e544abbeabd523e681f902a45f64d0e80448
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-30 13:04:48 -0300

    feat: create `Update` module for `User`

--- lib/exmeal/users/update.ex
+++ lib/exmeal/users/update.ex
@@ -1,7 +1,29 @@
 defmodule Exmeal.Users.Update do
-  alias Exmeal.{Error, Repo, User}
+  alias Exmeal.{Error, User, Repo}
+  alias Exmeal.Users.Get
 
   def call(%{"id" => id} = params) do
-    # TO DO
+    id
+    |> Get.by_id()
+    |> do_update(params)
+  end
+
+  defp do_update({:error, _reason} = result, _params), do: result
+
+  defp do_update({:ok, %User{} = user}, params) do
+    update_response =
+      user
+      |> User.changeset(params)
+      |> Repo.update()
+
+    case update_response do
+      {:ok, %User{}} = result ->
+        result
+
+      {:error, reason} ->
+        reason
+        |> Error.build_bad_request()
+        |> Error.wrap()
+    end
   end
 end
