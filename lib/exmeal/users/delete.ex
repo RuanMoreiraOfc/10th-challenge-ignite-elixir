a3cd8ddf328a994fdac36c545747ce039e326c19
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-30 13:05:04 -0300

    feat: create `Delete` module for `User`

--- lib/exmeal/users/delete.ex
+++ lib/exmeal/users/delete.ex
@@ -1,6 +1,14 @@
 defmodule Exmeal.Users.Delete do
+  alias Exmeal.{User, Repo}
+  alias Exmeal.Users.Get
 
-  def call() do
-    # TO DO
+  def call(id) do
+    case Get.by_id(id) do
+      {:ok, %User{} = user} ->
+        Repo.delete(user)
+
+      {:error, _reason} = result ->
+        result
+    end
   end
 end
