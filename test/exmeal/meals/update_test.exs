39c967ed2fb44af662f21372161ec9a9b7b25dc4
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-30 13:21:02 -0300

    fix(UpdateMealTest): add `user_id` field

--- test/exmeal/meals/update_test.exs
+++ test/exmeal/meals/update_test.exs
@@ -35,14 +35,15 @@ defmodule Exmeal.Meals.UpdateTest do
     end
 
     test "when a valid id is given, but wrong params, returns an error" do
-      params = %{
-        calories: 20,
-        date: ~D[2001-05-02],
-        description: "Banana"
-      }
-      {_ok, meal} = Exmeal.create_meal(params)
+      user_params = build(:users_params)
 
-      response = Exmeal.update_meal(%{"id" => meal.id, "date" => 0})
+      {:ok, %User{id: user_id}} = Exmeal.create_user(user_params)
+
+      params = build(:meals_params, %{user_id: user_id})
+
+      {:ok, %Meal{id: id}} = Exmeal.create_meal(params)
+
+      response = Exmeal.update_meal(%{"id" => id, "date" => 0})
 
       assert {:error, %Exmeal.Error{status: :bad_request}} = response
     end
