439976662109a3d968763ecc6a7ca6abe01085f3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-30 13:04:36 -0300

    feat: create `Create` module for `User`

--- lib/exmeal/users/create.ex
+++ lib/exmeal/users/create.ex
@@ -1,6 +1,20 @@
 defmodule Exmeal.Users.Create do
+  alias Exmeal.{Error, Repo, User}
 
-  def call() do
-    # TO DO
+  def call(params) do
+    insertion_response =
+      params
+      |> User.changeset()
+      |> Repo.insert()
+
+    case insertion_response do
+      {:ok, %User{}} = result ->
+        result
+
+      {:error, reason} ->
+        reason
+        |> Error.build_bad_request()
+        |> Error.wrap()
+    end
   end
 end
