8d22ecc6b561a5e15647f9cbcb25eb4d51c80a45
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-30 13:13:19 -0300

    feat: add common cases for `Users` view

--- lib/exmeal_web/views/users_view.ex
+++ lib/exmeal_web/views/users_view.ex
@@ -1,11 +1,16 @@
 defmodule ExmealWeb.UsersView do
   use ExmealWeb, :view
 
-  def render() do
-    # TO DO
-  end
+  alias Exmeal.User
 
-  def render() do
-    # TO DO
+  def render("user.json", %{user: %User{} = user}), do: %{user: user}
+
+  def render("create.json", %{user: %User{} = user}) do
+    %{
+      message: "User created!",
+      user: %{
+        user: user
+      }
+    }
   end
 end
